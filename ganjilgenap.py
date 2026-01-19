54430e2d6593b297e209a12c02f2267a42defce9
queenafrd <queenamedisyahputri@gmail.com>
2026-01-19 01:21:11 +0000

    versi 1.0 ganjilgenap

---
+++ ganjilgenap.py
@@ -0,0 +1,6 @@
+bil = int(input("Masukkan bilangan:"))
+
+if bil % 2 == 0:
+    print(bil,"Adalah GENAP")
+else:
+    print("kode ini saya buat dari codespace")
---
+++ main.py
@@ -0,0 +1,2 @@
+# ini adalah kode phyton pertama saya
+print("Halo saya sedang membuat proyek pertama di codespace")
